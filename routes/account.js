40d79fac014fb0aa40603f5ff63bad8e88bd0421
Admin <nghinhlehong@gmail.com>
2023-06-08 19:14:40 +0700

    update giao dien

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var accountcontroller =require('../controller/account.controller');
-var check_login = require('../middleware/check_login')
+var check_login = require('../middleware/check_login');
+const { get } = require('mongoose');
 
 
 router.get('/login', check_login.khong_yc_dang_nhap, accountcontroller.Login);
 router.post('/login', check_login.khong_yc_dang_nhap, accountcontroller.Login);
+
+// router.get('/logout',accountcontroller.Login);
+
 
 router.get('/reg', check_login.khong_yc_dang_nhap, accountcontroller.Reg);
 router.post('/reg', check_login.khong_yc_dang_nhap, accountcontroller.Reg);
