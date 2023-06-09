055b9126f7fa9e99cc6b89911597d613e579ae7e
viet <vietdhph26437@fpt.edu.vn>
2023-06-09 21:09:40 +0700

    them_anh

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(session({
   secret:'nhvhi3432j492j35nfdshfúydfy2h3nksjdfh9',
