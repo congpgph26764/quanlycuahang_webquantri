c5e40dcc7684c2b9da896178ce372ffc2586ee4e
Init project <congpgph26764@fpt.edu.vn>
2023-06-11 00:15:03 +0700

    update

--- models/model.js
+++ models/model.js
@@ -75,7 +75,7 @@ const feedbackSchema = new db.mongoose.Schema(
         phone: { type: String , required: true },
         email: { type: String , required: true },
         comment: { type: String , required: true }, 
-        question: { type: String , required: true },
+        image: { type: String , required: true },
     },
     {
         collection: 'feedback'
