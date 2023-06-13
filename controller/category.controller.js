60732ff71aed0d82b3571af3d9c570c024f30184
quangtai1804 <tailqph26595@fpt.edu.vn>
2023-06-13 14:57:37 +0700

    update edit cate

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -3,144 +3,162 @@ const db = require('../models/model');
 const path = require('path');
 
 
-exports.getHome = async (req,res,next)=>{
+exports.getHome = async (req, res, next) => {
     res.render('index');
 }
 
-exports.addCat = async(req,res,next)=>{
+exports.addCat = async (req, res, next) => {
     let msg = ''; // ghi câu thông báo
     var url_image = '';
     let image = "";
 
-    
-
     let listCat = await db.catModel.find();
 
-    
+    if (req.method == 'POST') {
 
-    if(req.method =='POST'){
 
-        
 
-        await fs.promises.rename(req.file.path,'./public/uploads/'+req.file.originalname)
-        url_image ='/uploads/'+req.file.originalname;
-        console.log("upload thành công"+url_image);
+        await fs.promises.rename(req.file.path, './public/uploads/' + req.file.originalname)
+        url_image = '/uploads/' + req.file.originalname;
+        console.log("upload thành công" + url_image);
 
         // Đường dẫn đến file ảnh
-        const imagePath = "./public"+url_image;
-        let image ="";
+        const imagePath = "./public" + url_image;
+        let image = "";
 
         try {
             const imageBuffer = fs.readFileSync(imagePath);
             const base64Image = imageBuffer.toString('base64');
             const fileExtension = path.extname(imagePath);
 
-            const dataUrl = 'data:image/'+fileExtension+';base64,'+base64Image;
+            const dataUrl = 'data:image/' + fileExtension + ';base64,' + base64Image;
             image = dataUrl;
-          } catch (error) {
+        } catch (error) {
             console.error('Lỗi khi chuyển đổi ảnh thành Base64:', error);
-          }
+        }
 
-        
+
         let objCat = new db.catModel();
         objCat.name = req.body.name;
         objCat.image = image;
-        
-        try{
+
+        try {
             let new_cat = await objCat.save();
             console.log(new_cat);
 
             console.log("Đã ghi thành công");
             msg = 'Đã thêm thành công';
-        }catch(err){
+        } catch (err) {
             console.log(err);
-            msg ='Lỗi '+ err.message;
+            msg = 'Lỗi ' + err.message;
 
         }
- 
+
     }
-    res.render( 'category/addcategory',{listCat:listCat})
+    res.render('category/addcategory', { listCat: listCat })
 }
-exports.listcat = async(req,res,next)=>{
-var list=await db.catModel.find();
-console.log(list);
+exports.listcat = async (req, res, next) => {
+    var list = await db.catModel.find();
+    console.log(list);
 
-res.render('category/category',{list:list})
+    res.render('category/category', { list: list })
 }
 
-exports.deleteCat = async (req,res,next)=>{
-let msg = ''; // chứa câu thông báo
-// load dữ liệu cũ để hiển thị
-let objCat = await db.catModel.findById(  req.params.idcat  );
-console.log( objCat);
-    
-    try{
-         
+exports.deleteCat = async (req, res, next) => {
+    let msg = ''; // chứa câu thông báo
+    // load dữ liệu cũ để hiển thị
+    let objCat = await db.catModel.findById(req.params.idcat);
+    console.log(objCat);
+
+    try {
+
         // update dữ liệu
         // await myModel.spModel.updateOne( {_id:  req.params.idsp},   objSP );
-         await db.catModel.findByIdAndDelete({_id:req.params.idcat});
-         res.redirect('/category');
+        await db.catModel.findByIdAndDelete({ _id: req.params.idcat });
+        res.redirect('/category');
 
         console.log("Đã xóa thành công");
         msg = 'Đã ghi thành công';
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        msg ='Lỗi '+ err.message;
+        msg = 'Lỗi ' + err.message;
 
     }
 
 
-res.render('category/category', 
-        {msg:msg})
+    res.render('category/category',
+        { msg: msg })
 
 }
-exports.editcat = async (req,res,next)=>{
-let msg = ''; // chứa câu thông báo
-// load dữ liệu cũ để hiển thị
-let objCat = await db.catModel.findById(  req.params.idcat  );
-console.log( objCat);
-if(req.method =='POST'){
+exports.editcat = async (req, res, next) => {
+    let msg = ''; // chứa câu thông báo
+    var url_image = '';
+    let image = "";
+    // load dữ liệu cũ để hiển thị
+    let objCat = await db.catModel.findById(req.params.idcat);
+    console.log(objCat);
+    if (req.method == 'POST') {
 
-    let objCat = new db.catModel();
-    objCat.name = req.body.name;
-    objCat.image = image;
-    objCat._id = req.params.idcat;
-    try{
-         
-        // update dữ liệu
-        // await myModel.spModel.updateOne( {_id:  req.params.idsp},   objSP );
-         await db.catModel.findByIdAndUpdate({_id:req.params.idcat},objCat);
-         res.redirect('/category');
-        console.log("Đã ghi thành công");
-        msg = 'Đã ghi thành công';
-     
-    }catch(err){
-        console.log(err);
-        msg ='Lỗi '+ err.message;
+        await fs.promises.rename(req.file.path, './public/uploads/' + req.file.originalname)
+        url_image = '/uploads/' + req.file.originalname;
+        console.log("upload thành công" + url_image);
+
+        // Đường dẫn đến file ảnh
+        const imagePath = "./public" + url_image;
+        let image = "";
+
+        try {
+            const imageBuffer = fs.readFileSync(imagePath);
+            const base64Image = imageBuffer.toString('base64');
+            const fileExtension = path.extname(imagePath);
+
+            const dataUrl = 'data:image/' + fileExtension + ';base64,' + base64Image;
+            image = dataUrl;
+        } catch (error) {
+            console.error('Lỗi khi chuyển đổi ảnh thành Base64:', error);
+        }
+
+
+        let objCat = new db.catModel();
+        objCat.name = req.body.name;
+        objCat.image = image;
+        objCat._id = req.params.idcat;
+        try {
+
+            // update dữ liệu
+            // await myModel.spModel.updateOne( {_id:  req.params.idsp},   objSP );
+            await db.catModel.findByIdAndUpdate({ _id: req.params.idcat }, objCat);
+            res.redirect('/category');
+            console.log("Đã ghi thành công");
+            msg = 'Đã ghi thành công';
+
+        } catch (err) {
+            console.log(err);
+            msg = 'Lỗi ' + err.message;
+
+        }
 
     }
 
-}
-
-res.render('category/editcategory', 
-        {msg:msg, objCat: objCat})
+    res.render('category/editcategory',
+        { msg: msg, objCat: objCat })
 
 }
-exports.sortcatname = async(req,res,next)=>{
-//Hiển thị danh sach san pham
+exports.sortcatname = async (req, res, next) => {
+    //Hiển thị danh sach san pham
 
-//kiểm tra tồn tại tham số
-let dieu_kien =null;
-if(typeof(req.query.name)!='undefined'){
-    let name =req.query.name;
-    dieu_kien={name:name};
-}
+    //kiểm tra tồn tại tham số
+    let dieu_kien = null;
+    if (typeof (req.query.name) != 'undefined') {
+        let name = req.query.name;
+        dieu_kien = { name: name };
+    }
 
 
-//var list=await myModel.spModel.find(dieu_kien).sort({name:1});
-//cair tieens lay them the loai
-var list=await db.catModel.find().sort({name:-1});
-console.log(list);
+    //var list=await myModel.spModel.find(dieu_kien).sort({name:1});
+    //cair tieens lay them the loai
+    var list = await db.catModel.find().sort({ name: -1 });
+    console.log(list);
 
-res.render('category/category',{list:list})
+    res.render('category/category', { list: list })
 }
