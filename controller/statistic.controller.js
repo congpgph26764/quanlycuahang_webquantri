67322b2eff6d9fee8bd149904101b1de289ab1a0
Init project <congpgph26764@fpt.edu.vn>
2023-06-21 23:04:48 +0700

    update

--- controller/statistic.controller.js
+++ controller/statistic.controller.js
@@ -9,36 +9,40 @@ exports.getHome = async (req, res, next) => {
     var totalByMonth = {'Tháng 1': 0, 'Tháng 2': 0, 'Tháng 3': 0, 'Tháng 4': 0, 'Tháng 5': 0, 'Tháng 6': 0,
     'Tháng 7': 0,'Tháng 8': 0,'Tháng 9': 0, 'Tháng 10': 0, 'Tháng 11': 0, 'Tháng 12': 0};
 
-    var year = [];
     var totalByYear = {};
 
     // Lặp qua từng đối tượng dữ liệu
     listbill.forEach(function(item) {
-    var dateParts = item.date.split("-"); // Tách ngày thành mảng
-    var month = "Tháng "+ parseInt(dateParts[1]); // Lấy phần tử thứ 2 là số tháng
-    var year = parseInt(dateParts[2]);  // Lấy phần tử thứ 4 là số năm
 
-    // Kiểm tra xem tổng cho tháng đã tồn tại chưa, nếu chưa thì khởi tạo
-    if (!totalByMonth[month]) {
-        totalByMonth[month] = 0;
-    }
+        
+        var dateParts = item.date.split("-"); // Tách ngày thành mảng
+        var month = "Tháng "+ parseInt(dateParts[1]); // Lấy phần tử thứ 2 là số tháng
+        var year1 = parseInt(dateParts[2]);  // Lấy phần tử thứ 4 là số năm
 
-    // Kiểm tra xem tổng cho năm đã tồn tại chưa, nếu chưa thì khởi tạo
-    if (!totalByYear[year]) {
-        totalByYear[year] = 0;
-    }
+        // Kiểm tra xem tổng cho tháng đã tồn tại chưa, nếu chưa thì khởi tạo
+        
+        if (!totalByMonth[month]) {
+            totalByMonth[month] = 0;
+        }
 
-    totalByMonth[month] += item.total_price; // Cộng tổng tiền cho tháng
-    totalByYear[year] += item.total_price; // Cộng tổng tiền cho năm
+        // Kiểm tra xem tổng cho năm đã tồn tại chưa, nếu chưa thì khởi tạo
+        if (!totalByYear[year1]) {
+            totalByYear[year1] = 0;
+            
+        }
+
+        if (year1 =="2023") {
+            totalByMonth[month] += item.total_price; // Cộng tổng tiền cho tháng
+        }
+        
+        totalByYear[year1] += item.total_price; // Cộng tổng tiền cho năm
+
     });
-    year.push(totalByYear)
+
+    const year = Object.entries(totalByYear).map(([key, value]) => ({ [key]: value }));
     
     console.log(totalByMonth);
-    console.log(totalByYear);
-    
-    
-
-
+    console.log(year);
 
     var listdbill = await db.detailed_billModel.find();
     
